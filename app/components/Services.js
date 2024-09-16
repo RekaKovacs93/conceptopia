678e219592e0e9db30e72cc5e3eeca752a9a7356
RekaKovacs93 <reka.kovacs1993@gmail.com>
2024-09-16 18:05:13 +0100

    fix

--- app/components/Services.js
+++ app/components/Services.js
@@ -32,14 +32,16 @@ export default function Services (){
     return(
         <div className="h-screen w-screen flex flex-wrap gap-5 items-center justify-center py-20">
             
-            {services.map((service, index) => (
+            {services.map((service, index) => (            
+                <div key={index} className=" h-48 w-96 md:h-48 md:w-96 rounded-2xl bg-dark bg-opacity-60">
+
                 <Link href={`/${encodeURIComponent(service.title.toLowerCase())}`} passHref>
-            <div key={index} className=" h-48 w-96 md:h-48 md:w-96 rounded-2xl bg-dark bg-opacity-60">
                 <div className="w-full h-full hover:scale-125 flex flex-col items-center justify-center">
                 <h1 id="title" className="text-3xl font-light opacity-100 text-center">{service.title}</h1>
                 </div>
-          </div>
           </Link>
+          </div>
+
         ))}
 
         </div>
