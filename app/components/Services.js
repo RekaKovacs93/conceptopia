5bf72031ca1a5a902b708da9e113dd62414b228e
RekaKovacs93 <reka.kovacs1993@gmail.com>
2025-01-20 20:01:23 +0000

    key

--- app/components/Services.js
+++ app/components/Services.js
@@ -37,7 +37,7 @@ export default function Services (){
             <h1 className="text-xl text-center text-dark mx-60 mb-20">Megoldások, nem csak szolgáltatások. Mi a Conceptopia Kreatív Ügynökségnél nem csupán a megszokott szolgáltatásokat kínálunk, hanem komplex megoldásokat, amelyek a vállalkozásod egyedi igényeihez igazodnak. Az általunk kínált megoldások nemcsak az eseti feladatok elvégzésére fókuszálnak, hanem arra is, hogy hosszú távú értéket teremtsenek a márkád számára. Legyen szó weboldal fejlesztésről, közösségi média stratégiáról vagy grafikai tervezésről, célunk, hogy egy átfogó, integrált megközelítést alkalmazzunk, amely segít kiemelkedni a versenytársak közül. Ezzel nemcsak időt és energiát takarítasz meg, hanem biztos lehetsz abban is, hogy minden részlet harmonikusan illeszkedik a vállalkozásod összképébe.</h1>
             <div className=" w-screen flex flex-wrap gap-5 items-center justify-center px-40">
                 {services.map((service, index) => (            
-                    <div key={service.title} className="h-48 w-96 md:w-96 rounded-2xl">
+                    <div key={index} className="h-48 w-96 md:w-96 rounded-2xl">
 
                 <Link href={`/${encodeURIComponent(service.title.toLowerCase())}`} passHref>
                 {/* <div className="w-full h-full hover:scale-125 flex flex-col items-center justify-center">
